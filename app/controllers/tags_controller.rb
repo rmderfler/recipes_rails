c29d5e8f0ba2e25b2200ad28ac695d7526706f26
Jeff Stringer & Michael Derfler <student@epicodus.com>
2014-08-21 15:37:48 -0700

    add has_many_belongs_to tags and recipes

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -18,4 +18,9 @@ class TagsController < ApplicationController
       render('tags/new.html.erb')
     end
   end
+
+  def show
+    @tag = Tag.find(params[:id])
+    render('tags/show.html.erb')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   match('tags', {:via => :get, :to => 'tags#index'})
   match('tags', {:via => :post, :to => 'tags#create'})
   match('tags/new', {:via => :get, :to => 'tags#new'})
+  match('tags/:id', {:via => :get, :to => 'tags#show'})
 end
